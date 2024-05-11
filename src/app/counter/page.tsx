97e270c15ff24a3ca8865fa3d434502b9aae55a0
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-05-11 23:10:19 +0530

    feat: added write counter

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useReadContract } from "wagmi";
 import { counterAddress, counterAbi } from "@/constants";
+import { WriteContract } from "@/components/WriteCounter";
 
 function Counter() {
   const {
@@ -14,9 +15,17 @@ function Counter() {
     functionName: "number",
   });
   return (
-    <div>
-      {isLoading ? <div>Loading</div> : <p>Counter: {counter?.toString()}</p>}
-    </div>
+    <main>
+      <section className="py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">Example Counter DApp</h1>
+        <p className="text-2xl text-muted-foreground">
+          {isLoading ? <>Loading...</> : <>Counter: {counter?.toString()}</>}
+        </p>
+      </section>
+      <div className="flex gap-6 items-center justify-center">
+        <WriteContract />
+      </div>
+    </main>
   );
 }
 
