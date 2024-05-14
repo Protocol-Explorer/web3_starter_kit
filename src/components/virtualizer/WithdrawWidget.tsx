cecc52356e1c7dd615407de71e2ad97205f08c52
Christopher Ramnath <dariusramnath6@gmail.com>
2024-05-14 12:24:08 -0400

    (Fix): changes to virtualizer page

--- src/components/virtualizer/WithdrawWidget.tsx
+++ src/components/virtualizer/WithdrawWidget.tsx
@@ -4,16 +4,13 @@ import InputComponent from "@/components/virtualizer/Input";
 
 export default function DepositWidget() {
   const [mUSDC, setmUSDC] = useState<number>(0);
-  const [vUSD, setvUSD] = useState<number>(0);
+//   const [vUSD, setvUSD] = useState<number>(0);
   return (
     <div >
       <div className="flex rounded-2xl items-left flex-col flex-grow pt-4">
         <div>
           <div className={`flex mx-2 text-accent`}>
             <InputComponent label="mUSDC" type="withdraw" initialValue={mUSDC} onValueChange={setmUSDC}/>
-          </div>
-          <div className={`flex mx-2 text-accent`}>
-            <InputComponent label="vUSD" type="withdraw" initialValue={vUSD} onValueChange={setvUSD}/>
           </div>
         </div>
       </div>
