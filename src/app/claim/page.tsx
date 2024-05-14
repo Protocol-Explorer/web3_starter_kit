440ffb94bd3657c34426ee519be07465f8d1bf1d
MattL28 <alleynematthaus@gmail.com>
2024-05-14 17:24:40 -0400

    added approval contract call to approve button

--- src/app/claim/page.tsx
+++ src/app/claim/page.tsx
@@ -21,9 +21,42 @@ import InputComponent from "@/components/InputWidget";
 export default function Claim() {
   const {address}=useAccount();
   const {open}=useWeb3Modal();
+  const [contractName,setcontractName]=useState<string>("");
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState<Error|null>(null);
   const handleConnect = () => {
     open();
   };
+  /*interface ReadContractCall{
+    data?:string;
+    error?:any;
+    isError?:boolean;
+    isLoading?:boolean;
+  }
+  const handleApproveCall = async ()=>{
+    console.log('handling happening');
+    try{
+      setLoading(true);
+      setError(null);
+
+      const result:ReadContractCall= await useWriteContract({
+        address:"0xbCCc252A134cEf81be20DF52F27D9029507F3605",
+        abi:MUSD_CONTRACT,
+        functionName:'approve',
+        args:[address,"10000000000000000000"]
+      })
+      if(result.isError){
+        throw new Error(result.error)
+      }
+      setcontractName(result.data || '')
+    }catch(error){
+      setError(error instanceof Error ? error : new Error(String(error)));
+      console.log(error)
+    }finally{
+      setLoading(false);
+    }
+  };*/
+  const {writeContract}=useWriteContract();
   return (
     <main className='flex flex-row justify-center align-center'>
         <Card className='max-w-md mx-auto rounded-3xl lg:mt-0 mt-14 bg-background'>
@@ -32,19 +65,17 @@ export default function Claim() {
                 <Button onClick={handleConnect}>Connect Wallet</Button>
                 ) : (
                 <>
-                  {/* <Button
-                    onClick={() =>
-                      sendTransaction({
-                        to: "0x1a343eFB966E63bfA25A2b368455448f02466Ffc",
-                        value: parseEther("0.1"),
-                      })
-                    }
-                    disabled={isConfirming}
-                    variant={"secondary"}
+                  <Button
+                    onClick={()=>writeContract({
+                      abi:MUSD_CONTRACT,
+                      address:"0xbCCc252A134cEf81be20DF52F27D9029507F3605",
+                      functionName:'approve',
+                      args:["0x38776466c8Fd3bA4f4bAc99403aDFA381C4e2e43",parseEther("10")]
+                    })}
+                    className='rounded-2xl px-6"'
                   >
-                    Swap
-                  </Button> */}
-                  <Button className="rounded-2xl px-6">Approve</Button>
+                    Approve
+                  </Button>
                   <Button className="rounded-2xl px-6 mt-12">Claim</Button>
                 </>
               )}
